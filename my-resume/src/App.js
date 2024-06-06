55a3308713533a3153fd65a894129b23dc5299f6
Andrey Melchihin <117808744+melchikhinfa@users.noreply.github.com>
2024-06-06 20:11:19 +0000

    Add css-styles and .js code

---
+++ my-resume/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Resume from './Resume';
+
+function App() {
+    return (
+        <div className="App">
+            <Resume />
+        </div>
+    );
+}
+
+export default App;
